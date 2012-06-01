b0a9b8a0f709000dade1c52f53c6d8eff2cc7f3e
Alvaro del Castillo <acs@barrapunto.com>
2012-06-02 00:00:47 +0200

    We have the tickets graph!

--- bitergiametrics/nf/metrics/swscopio.js
+++ bitergiametrics/nf/metrics/swscopio.js
@@ -1,242 +1,216 @@
-// Display timeseries for tickets
-// the finance envision template
-function displayEvoTickets (id, datafile) {
+function displayEvoTickets(id, datafile) {
 
-    var
-    container = document.getElementById(id);
+	var container = document.getElementById(id);
 
-    $.getJSON(datafile, function (history) {
+	$.getJSON(datafile, function(history) {
 
-	var
-	V = envision,
-	firstMonth = history.id[0],
-	tickets = [history.id,history.tickets],
-	dates = history.date,
-	options, vis;
-	
-	alert (tickets);
+		var data, options, i;
 
-	options = {
-	    container : container,
-	    data : {
-		price : tickets,
-	    },
-	    trackFormatter : function (o) {
-		
-		var
-//		index = o.index,
-		data = o.series.data,
-		index = data[o.index][0]-firstMonth,
-		value;
-		
-		value = dates[index] + ": " + commits[1][index] + " commits, " + committers[1][index] + " committers (commits per committer: " + ratio[1][index] + ")";
+		// Data Format:
+		data = [ [ history.id, history.tickets ] ];
 
-		return value;
-	    },
-	    xTickFormatter : function (index) {
-		return Math.floor(index/12) + '';
-	    },
-	    yTickFormatter : function (n) {
-		return n + '';
-	    },
-	    // Initial selection
-	    selection : {
-		data : {
-		    x : {
-			min : history.id[0],
-			max : history.id[history.id.length - 1]
-		    }
-		}
-	    }
-	};
+		// TimeSeries Template Options
+		options = {
+			// Container to render inside of
+			container : container,
+			// Data for detail (top chart) and summary (bottom chart)
+			data : {
+				detail : data,
+				summary : data
+			},
+			// Initial selection
+			selection : {
+				data : {
+					x : {
+						min : history.id[0],
+						max : history.id[history.month.length - 1]
+					}
+				}
+			}
+		};
 
-        // Create the TimeSeries
-	vis = new envision.templates.Finance(options);
-    });
+		// Create the TimeSeries
+		new envision.templates.TimeSeries(options);
+	});
 }
-
-
-
 
 // Display timeseries for commits and committers using
 // the finance envision template
-function displayEvoCommits (id, datafile) {
+function displayEvoCommits(id, datafile) {
 
-    var
-    container = document.getElementById(id);
+	var container = document.getElementById(id);
 
-    $.getJSON(datafile, function (history) {
+	$.getJSON(datafile,
+			function(history) {
 
-	var
-	V = envision,
-	firstMonth = history.id[0],
-	commits = [history.id,history.commits],
-	committers = [history.id,history.committers],
-	ratio = [history.id,history.ratio],
-	dates = history.date,
-	options, vis;
+				var V = envision, firstMonth = history.id[0], commits = [
+						history.id, history.commits ], committers = [
+						history.id, history.committers ], ratio = [ history.id,
+						history.ratio ], dates = history.date, options, vis;
 
-	options = {
-	    container : container,
-	    data : {
-		price : commits,
-		volume : committers,
-		summary : commits
-	    },
-	    trackFormatter : function (o) {
-		
-		var
-//		index = o.index,
-		data = o.series.data,
-		index = data[o.index][0]-firstMonth,
-		value;
-		
-		value = dates[index] + ": " + commits[1][index] + " commits, " + committers[1][index] + " committers (commits per committer: " + ratio[1][index] + ")";
+				options = {
+					container : container,
+					data : {
+						price : commits,
+						volume : committers,
+						summary : commits
+					},
+					trackFormatter : function(o) {
 
-		return value;
-	    },
-	    xTickFormatter : function (index) {
-		return Math.floor(index/12) + '';
-	    },
-	    yTickFormatter : function (n) {
-		return n + '';
-	    },
-	    // Initial selection
-	    selection : {
-		data : {
-		    x : {
-			min : history.id[0],
-			max : history.id[history.id.length - 1]
-		    }
-		}
-	    }
-	};
+						var
+						// index = o.index,
+						data = o.series.data, index = data[o.index][0]
+								- firstMonth, value;
 
-        // Create the TimeSeries
-	vis = new envision.templates.Finance(options);
-    });
+						value = dates[index] + ": " + commits[1][index]
+								+ " commits, " + committers[1][index]
+								+ " committers (commits per committer: "
+								+ ratio[1][index] + ")";
+
+						return value;
+					},
+					xTickFormatter : function(index) {
+						return Math.floor(index / 12) + '';
+					},
+					yTickFormatter : function(n) {
+						return n + '';
+					},
+					// Initial selection
+					selection : {
+						data : {
+							x : {
+								min : history.id[0],
+								max : history.id[history.id.length - 1]
+							}
+						}
+					}
+				};
+
+				// Create the TimeSeries
+				vis = new envision.templates.Finance(options);
+			});
 }
 
 // Display timeseries for commits/committers ratio
 // using the timeseries envision template
-function displayCommitsRatio (id, datafile) {
+function displayCommitsRatio(id, datafile) {
 
-    var
-    container = document.getElementById(id);
+	var container = document.getElementById(id);
 
-    $.getJSON(datafile, function (history) {
+	$.getJSON(datafile, function(history) {
 
-        var
-        data, options, i;
-	
-        // Data Format:
-        data = [
-            [history.id, history.ratio]
-        ];
-	
-        // TimeSeries Template Options
-        options = {
-            // Container to render inside of
-            container : container,
-            // Data for detail (top chart) and summary (bottom chart)
-            data : {
-		detail : data,
-		summary : data
-            },
-	    // Initial selection
-	    selection : {
-		data : {
-		    x : {
-			min : history.id[0],
-			max : history.id[history.month.length - 1]
-		    }
-		}
-	    }
-        };
-	
-        // Create the TimeSeries
-        new envision.templates.TimeSeries(options);
-    });
+		var data, options, i;
+
+		// Data Format:
+		data = [ [ history.id, history.ratio ] ];
+
+		// TimeSeries Template Options
+		options = {
+			// Container to render inside of
+			container : container,
+			// Data for detail (top chart) and summary (bottom chart)
+			data : {
+				detail : data,
+				summary : data
+			},
+			// Initial selection
+			selection : {
+				data : {
+					x : {
+						min : history.id[0],
+						max : history.id[history.month.length - 1]
+					}
+				}
+			}
+		};
+
+		// Create the TimeSeries
+		new envision.templates.TimeSeries(options);
+	});
 }
 
 // Display timeseries for commits and committers using
 // the finance envision template
-function displayEvoLines (id, datafile, show) {
+function displayEvoLines(id, datafile, show) {
 
-    var
-    container = document.getElementById(id);
-    container.innerHTML = "";
+	var container = document.getElementById(id);
+	container.innerHTML = "";
 
-    $.getJSON(datafile, function (history) {
+	$
+			.getJSON(
+					datafile,
+					function(history) {
 
-	var
-	V = envision,
-	firstMonth = history.id[0],
-	commits = [history.id,history.commits],
-	added = [history.id,history.added],
-	removed = [history.id,history.removed],
-	ratio = [history.id,history.ratio],
-	dates = history.date,
-	options, vis;
+						var V = envision, firstMonth = history.id[0], commits = [
+								history.id, history.commits ], added = [
+								history.id, history.added ], removed = [
+								history.id, history.removed ], ratio = [
+								history.id, history.ratio ], dates = history.date, options, vis;
 
-	if (show == "removed") {
-	    price = removed
-	} else if (show == "added") {
-	    price = added
-	} else if (show == "added,removed") {
-	    price = [added,removed]
-	} else {
-	    price = [added,removed]
-	}
-	options = {
-	    container : container,
-	    data : {
-		price : price,
-		volume : ratio,
-		summary : commits
-	    },
-	    trackFormatter : function (o) {
-		
-		var
-		data = o.series.data,
-		index = data[o.index][0]-firstMonth,
-		value;
-		
-		
-		value = dates[index] + ' (' + commits[1][index] + " commits): ";  		 if (show == "removed") {
-		    value += removed[1][index] + " lines removed, "
-		} else if (show == "added") {
-		    value += added[1][index] + " lines added, "
-		} else if (show == "added,removed") {
-		    value += added[1][index] + " lines added, " + removed[1][index] + " removed, "
-		} else {
-		    value += added[1][index] + " lines added, " + removed[1][index] + " removed, "
-		}
+						if (show == "removed") {
+							price = removed
+						} else if (show == "added") {
+							price = added
+						} else if (show == "added,removed") {
+							price = [ added, removed ]
+						} else {
+							price = [ added, removed ]
+						}
+						options = {
+							container : container,
+							data : {
+								price : price,
+								volume : ratio,
+								summary : commits
+							},
+							trackFormatter : function(o) {
 
-		value += ratio[1][index] + " lines changed / commit";
+								var data = o.series.data, index = data[o.index][0]
+										- firstMonth, value;
 
-//		value = dates[index] + ' (' + commits[1][index] + " commits): " + added[1][index] + " lines added, " + ratio[1][index] + " lines changed / commit";
+								value = dates[index] + ' (' + commits[1][index]
+										+ " commits): ";
+								if (show == "removed") {
+									value += removed[1][index]
+											+ " lines removed, "
+								} else if (show == "added") {
+									value += added[1][index] + " lines added, "
+								} else if (show == "added,removed") {
+									value += added[1][index] + " lines added, "
+											+ removed[1][index] + " removed, "
+								} else {
+									value += added[1][index] + " lines added, "
+											+ removed[1][index] + " removed, "
+								}
 
-		return value;
-	    },
-	    xTickFormatter : function (index) {
-		return Math.floor(index/12) + '';
-	    },
-	    yTickFormatter : function (n) {
-		return n + '';
-	    },
-	    // Initial selection
-	    selection : {
-		data : {
-		    x : {
-			min : history.id[0],
-			max : history.id[history.month.length - 1]
-		    }
-		}
-	    }
-	};
+								value += ratio[1][index]
+										+ " lines changed / commit";
 
-        // Create the TimeSeries
-	vis = new envision.templates.Finance(options);
-    });
+								// value = dates[index] + ' (' +
+								// commits[1][index] + " commits): " +
+								// added[1][index] + " lines added, " +
+								// ratio[1][index] + " lines changed / commit";
+
+								return value;
+							},
+							xTickFormatter : function(index) {
+								return Math.floor(index / 12) + '';
+							},
+							yTickFormatter : function(n) {
+								return n + '';
+							},
+							// Initial selection
+							selection : {
+								data : {
+									x : {
+										min : history.id[0],
+										max : history.id[history.month.length - 1]
+									}
+								}
+							}
+						};
+
+						// Create the TimeSeries
+						vis = new envision.templates.Finance(options);
+					});
 }
